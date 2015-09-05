cf49d59fd63c9e964ced652166979f672cda4731
Shinichi Inoue <inoue.shinichi.1800@gmail.com>
2015-09-05 17:27:45 +0900

    熱電対式温度センサプログラム

--- tempXBeeRouter/tempXBeeRouter.ino
+++ tempXBeeRouter/tempXBeeRouter.ino
@@ -1,15 +1,11 @@
-/*
-*XBee APIフレーム用送受信プログラム
-*随時更新していきます。
-*現在できている項目
-*  1: 宛先Addressの設定
-*  2: 通常データの送受信
-*  3: ATコマンドによるホストXBeeへの要求と応答の受け取り
-*
-*/
+//       uno  mega
+//digital 10   53  :SS
+//digital 11   51  :MOSI
+//digital 12   50  :MISO
+//digital 13   52  :SCK
 
-//#define MEGA
-#define EDISON
+#define MEGA
+//#define EDISON
 
 //#define ROUTER1
 //#define ROUTER2
@@ -20,6 +16,8 @@
 #include <SoftwareSerial.h>
 #endif
 #include "ev86XBee.h"   
+#include <SPI.h>
+
 
 #ifdef MEGA
 int rx = 10;
@@ -45,6 +43,7 @@ EV86XBeeR router = EV86XBeeR();
 
 
 // プロトタイプ宣言
+void tmp(int x);
 String float2String(float value);
 
 String request = "request";    // コールバック用変数
@@ -64,17 +63,36 @@ String startAck = "startAck3";
 String senData ="ROUTER3sensor";
 #endif
 
-// 水温センサパラメータ
-float B = 3435;
-float T0 = 298.15;
-float R0 = 10.0;
-float R1 = 10.0;
-int n1,n2;
-float rr1,rr2,t1,t2,temp1,temp2;
+
+// SPI & 熱電対式温度センサーパラメータ
+#define SLAVE1 53   //uno 10 mega 53
+#define SLAVE2 49   //uno  9 mega 49
+unsigned int thermocouple;  // 14-Bit Thermocouple Temperature Data + 2-Bit
+unsigned int internal;      // 12-Bit Internal Temperature Data + 4-Bit
+float tempTime, tempTime2;
+float disp, disp2;                 // display value
+char s[10], s2[10]; // 時間
+char c[10], c2[10];// 温度
 
 
-void setup() {
-  Serial.begin(9600);                          // Arduino-PC間の通信
+void setup(){
+  
+  // SPI用ピン設定
+  pinMode(SLAVE1, OUTPUT);
+  digitalWrite(SLAVE1, HIGH);
+  pinMode(SLAVE2, OUTPUT);
+  digitalWrite(SLAVE2, HIGH);
+  
+  // SPIプロトコル設定
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  SPI.setDataMode(SPI_MODE0);
+  
+  // PC−Arduino間Serial
+  Serial.begin(9600);
+  
+  // Arduino-XBee間Serial
 #ifdef MEGA
   myserial.begin(9600);
   myserial.flush();
@@ -86,8 +104,8 @@ void setup() {
   Serial1.flush();
   router.begin(Serial1);
 #endif
-  delay(5000); 
-  
+  delay(5000);
+ 
   // ホストXBeeの設定確認用メソッド
   router.hsXBeeStatus();                     
   delay(2000);
@@ -96,41 +114,21 @@ void setup() {
   router.setDstAdd64(coor.h64Add, coor.l64Add);
   router.rmXBeeStatus();
   Serial.println("Finish checking destination xbee node parameters");
-  delay(2000);
+  delay(2000); 
+  
 }
 
-void loop() {
+void loop(){
   Serial.println("-----------------------------");
   // 受信データの初期化
   router.clearData();
   
-  //温度センサ読み取り処理
-  n1 = analogRead(1);
-  n2 = analogRead(2);
+  // 温度取得
+  tmp(SLAVE1);
+  tmp(SLAVE2);
   
-  rr1 = R1 * n1 / (1024.0 - n1);
-  rr2 = R1 * n2 / (1024.0 - n2);
-  
-  t1 = 1 / (log(rr1 / R0) / B + (1 / T0));
-  t2 = 1 / (log(rr2 / R0) / B + (1 / T0));
-  
-  temp1 = t1 - 273.15;
-  temp2 = t2 - 273.15;
-  
-  Serial.print("rr1:");
-  Serial.print(rr1);
-  Serial.print(" Temp1:");
-  Serial.println(temp1);
-  
-  Serial.print("rr2:");
-  Serial.print(rr2);
-  Serial.print(" Temp2:");
-  Serial.println(temp2);
-  
-  // 送信用データに変換
-  senData = float2String(temp1);
-  
-  delay(1000);
+  // 送信用データに変換 (Temp1のみ)
+  senData = float2String(disp);
   
   // 受信パケットの確認
   Serial.println("[get Packet]");
@@ -180,6 +178,66 @@ void loop() {
   // 受信データの初期化
   router.clearData();
   delay(300);
+}
+
+// 熱電対式温度センサ取得関数
+void tmp(int x){
+  digitalWrite(x,LOW);                                      //  Enable the chip
+  thermocouple  = (unsigned int)SPI.transfer(0x00) << 8;   //  Read high byte thermocouple
+  thermocouple |= (unsigned int)SPI.transfer(0x00);        //  Read low byte thermocouple 
+  internal  = (unsigned int)SPI.transfer(0x00) << 8;       //  Read high byte internal
+  internal |= (unsigned int)SPI.transfer(0x00);            //  Read low byte internal 
+  digitalWrite(x, HIGH);                                   //  Disable the chip
+  
+  if((thermocouple & 0x0001) != 0) {
+    Serial.print("ERROR: ");
+    if ((internal & 0x0004) !=0)  Serial.print("Short to Vcc, ");
+    if ((internal & 0x0002) !=0)  Serial.print("Short to GND, ");
+    if ((internal & 0x0001) !=0)  Serial.print("Open Circuit, ");
+    Serial.println();
+  }
+  else {
+    if((thermocouple & 0x8000) == 0){ // 0℃以上   above 0 Degrees Celsius 
+      
+      if (x == SLAVE1) {
+        disp = (thermocouple >> 2) * 0.25;
+      } else if (x == SLAVE2) {
+         disp2 = (thermocouple >> 2) * 0.25;
+      }
+      
+    }
+    else {                            // 0℃未満   below zero
+      if (x == SLAVE1) {
+        disp = (0x3fff - (thermocouple >> 2) + 1)  * -0.25;
+      } else if (x == SLAVE2) {
+        disp2 = (0x3fff - (thermocouple >> 2) + 1)  * -0.25;
+      }
+      
+    }
+    
+    if (x == SLAVE1) {
+      tempTime = millis();
+      tempTime /= 1000;
+      dtostrf(tempTime, 6, 3, s);  //時間
+      dtostrf(disp, 6, 2, c);  //温度
+      Serial.print(s);
+      Serial.print(";TMP1;");
+      Serial.print(c);
+      Serial.print(";");
+      Serial.println();    
+    } else if (x == SLAVE2) {
+      tempTime2 = millis();
+      tempTime2 /= 1000;
+      dtostrf(tempTime2, 6, 3, s);  //時間
+      dtostrf(disp2, 6, 2, c);  //温度
+      Serial.print(s2);
+      Serial.print(";TMP2;");
+      Serial.print(c2);
+      Serial.print(";");
+      Serial.println();  
+    }
+  }
+  delay(500);
 }
 
 // 変換 [float->string] 
